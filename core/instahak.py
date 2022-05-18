213d0ab0cad4dda2dd68491a976196410b06fe6a
brookehorizon <danieltashman17@gmail.com>
2022-05-18 22:35:03 +0300

    added

---
+++ core/instahak.py
@@ -0,0 +1,46 @@
+import requests
+from  datetime import  datetime
+import json
+import dearpygui.dearpygui as dpg
+
+
+class InstaHak:
+
+    def login(self, username, password):
+        link = 'https://www.instagram.com/accounts/login/'
+        login_url = 'https://www.instagram.com/accounts/login/ajax/'
+
+        time = int(datetime.now().timestamp())
+        response = requests.get(link)
+
+        payload = {
+            'username': username,
+            'enc_password': f'#PWD_INSTAGRAM_BROWSER:0:{time}:{password}',
+            'queryParams': {},
+            'optIntoOneTap': 'false'
+        }
+
+        login_header = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36",
+            "X-Requested-With": "XMLHttpRequest",
+            "Referer": "https://www.instagram.com/accounts/login/",
+            "x-csrftoken": response.cookies['csrftoken']
+        }
+
+        login_response = requests.post(login_url, data=payload, headers=login_header)
+        json_data = json.loads(login_response.text)
+        print(json_data)
+        try:
+            if json_data["authenticated"]:
+                print("login successful", password)
+                cookies = login_response.cookies
+                cookie_jar = cookies.get_dict()
+                csrf_token = cookie_jar['csrftoken']
+                print("csrf_token: ", csrf_token)
+                session_id = cookie_jar['sessionid']
+                print("session_id: ", session_id)
+                return True
+            else:
+                return False
+        except:
+            return False
